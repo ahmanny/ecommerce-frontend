174b9047675eecd9e0741a34b238be1c81166caa
Ahman Solomon <ahmansolomon@gmail.com>
2025-05-27 10:21:48 +0100

    4th commit

--- src/components/admin/productsManagement/AddProductForm.tsx
+++ src/components/admin/productsManagement/AddProductForm.tsx
@@ -2,12 +2,20 @@
 
 import { useCreateProduct } from "@/services/products/productQueries";
 import ProductForm from "./ProductFormT";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function AddProductForm() {
   const { mutate, isPending, isError, error } = useCreateProduct();
+  const router = useRouter();
 
   const handleAddProduct = (formData: FormData) => {
-    mutate(formData);
+    mutate(formData, {
+      onSuccess: () => {
+        toast.success("Product updated successfully!");
+        router.push("/admin/products");
+      },
+    });
   };
 
   return (
